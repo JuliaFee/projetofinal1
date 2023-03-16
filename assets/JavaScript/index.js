e1eb3e66a17fdac1cfa989bf8a87af269215a58a
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-03-16 15:34:31 -0300

    page index

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -38,18 +38,3 @@ btnPrev.addEventListener('click', prevSlider)
 
 console.log(slider)
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-});
