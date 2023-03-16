91237fd54ceecad76dd942c5507ba0bf8cea8b3e
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-03-16 08:20:33 -0300

    index

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -1,1 +1,13 @@
-const slider = document.querySelectorAll('slider')
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+    slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+    slider[currentSlide].classList.add('on')
+}
