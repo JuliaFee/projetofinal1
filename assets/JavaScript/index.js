34f1cbb011d4a073c448c971719c3e773a741390
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-03-21 10:25:59 -0300

    page index

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -1,8 +1,8 @@
-const productContainers = [...document.querySelectorAll('.cardm-container')];
+const moviesContainers = [...document.querySelectorAll('.cardm-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-productContainers.forEach((item, i) => {
+moviesContainers.forEach((item, i) => {
     let containerDimensions = item.getBoundingClientRect();
     let countainerWidth = containerDimensions.width;
 
