ffb12a40386abaec2aeadb51d454e8ed8a1db1d0
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-03-21 10:23:56 -0300

    page index

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -1,40 +1,16 @@
-const slider = document.querySelectorAll('.slider');
-const btnPrev = document.getElementById('prev-button');
-const btnNext = document.getElementById('next-button');
+const productContainers = [...document.querySelectorAll('.cardm-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-let currentSlide = 0;
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let countainerWidth = containerDimensions.width;
 
-function hideSlider() {
-    slider.forEach(item => item.classList.remove('on'))
-}
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += countainerWidth;
+    })
 
-function showSlider() {
-    slider[currentSlide].classList.add('on')
-}
-
-function nextSlider() {
-    hideSlider ()
-    if(currentSlide ===  slider.length -1) {
-        currentSlide = 0
-    } else {
-        currentSlide++
-    }
-    showSlider()
-}
-
-function prevSlider() {
-    hideSlider ()
-    if(currentSlide === 0) {
-        currentSlide = slider.length -1
-    } else {
-        currentSlide--
-    }
-    showSlider()
-}
-
-btnNext.addEventListener('click', nextSlider)
-btnPrev.addEventListener('click', prevSlider)
-
-
-console.log(slider)
-
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= countainerWidth;
+    })
+})
