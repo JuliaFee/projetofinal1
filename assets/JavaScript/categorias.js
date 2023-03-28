33c8573104ae52877b2575f8d50cec9f437a2e59
IsaBarquilia <isabelle.barquilia.564@gmail.com>
2023-03-28 16:39:28 -0300

    page categorics

--- assets/JavaScript/categorias.js
+++ assets/JavaScript/categorias.js
@@ -1,16 +1,28 @@
-const moviesContainers = [...document.querySelectorAll('.cardm-container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-moviesContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let countainerWidth = containerDimensions.width;
+!(function ($doc, $win) {
+	var screenWidth = $win.screen.width / 2,
+		screenHeight = $win.screen.height / 2,
+		$elems = $doc.getElementsByClassName("elem"),
+		validPropertyPrefix = '',
+		otherProperty = 'perspective(1000px)',
+		elemStyle = $elems[0].style;
 
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += countainerWidth;
-    })
+	if(typeof elemStyle.webkitTransform == 'string') {
+		validPropertyPrefix = 'webkitTransform';
+	} else if (typeof elemStyle.MozTransform == 'string') {
+		validPropertyPrefix = 'MozTransform';
+	}
 
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= countainerWidth;
-    })
-})
+	$doc.addEventListener('mousemove', function (e) {
+		var centroX = e.clientX - screenWidth,
+			centroY = screenHeight - (e.clientY + 13),
+			degX = centroX * 0.04,
+			degY = centroY * 0.02,
+			$elem
+
+		for (var i = 0; i < $elems.length; i++) {
+   			$elem = $elems[i];
+			$elem.style[validPropertyPrefix] = otherProperty + 'rotateY('+ degX +'deg)  rotateX('+ degY +'deg)';
+		};
+	});
+})(document, window);
